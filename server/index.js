16dcc1a23a4727b00a8155d545d673589950a066
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2025-08-09 17:48:32 +0100

    update port number

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use("/menu", menuRouter);
 
 app.use((_req, res) => res.status(404).json({ error: "Route not found" }));
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`API running on http://localhost:${port}`);
 });
